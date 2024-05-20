96fe2d8eb118d147f6b859b9307dba1fa2c43bf8
Lenin Guaminga <leninguaminga@gmail.com>
2024-05-20 00:50:40 -0500

    Optimizacion🥲🪴

---
+++ config.lua
@@ -0,0 +1,107 @@
+reload("lnn.options")
+
+-- Enable powershell as your default shell
+vim.opt.shell = "pwsh.exe"
+vim.opt.shellcmdflag =
+"-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+vim.cmd [[
+		let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+		let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+		set shellquote= shellxquote=
+  ]]
+
+-- Set a compatible clipboard manager
+vim.g.clipboard = {
+  copy = {
+    ["+"] = "win32yank.exe -i --crlf",
+    ["*"] = "win32yank.exe -i --crlf",
+  },
+  paste = {
+    ["+"] = "win32yank.exe -o --lf",
+    ["*"] = "win32yank.exe -o --lf",
+  },
+}
+
+-- MY KEYMAPS -----------------------------------------------------------------
+-- Agregar keymap para copiar con Ctrl+c
+vim.api.nvim_set_keymap('n', '<C-c>', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-c>', '"+y', { noremap = true, silent = true })
+-- Agregar keymap para seleccionar todo el documento con Ctrl+a
+vim.api.nvim_set_keymap('n', '<C-a>', 'ggVG', { noremap = true, silent = true })
+-- Define una variable global para rastrear el estado de relativenumber
+lvim.relativenumber_enabled = false
+
+-- Agrega un mapeo de teclas para alternar relativenumber al presionar Alt + n
+lvim.keys.normal_mode["<A-n>"] = function()
+  if lvim.relativenumber_enabled then
+    vim.cmd("set norelativenumber")
+    lvim.relativenumber_enabled = false
+  else
+    vim.cmd("set relativenumber")
+    lvim.relativenumber_enabled = true
+  end
+end
+
+--CUSTOM PLUGINS -----------------------------------------------------------------
+lvim.plugins = {
+  --auto-save
+  {
+    "okuuva/auto-save.nvim",
+    cmd = "ASToggle",                         -- optional for lazy loading on command
+    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+    opts = {
+      -- your config goes here
+      -- or just leave it empty :)
+    },
+  },
+  { "kevinhwang91/nvim-ufo", dependencies = "kevinhwang91/promise-async" },
+  { "nvim-treesitter/nvim-treesitter-angular" },
+}
+
+--CUSTOM PLUGINS CONFIG-----------------------------------------------
+-- visual folding config
+vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+
+-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
+-- Option 2: nvim lsp as LSP client
+-- Tell the server the capability of foldingRange,
+-- Neovim hasn't added foldingRange to default capabilities, users must add it manually
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.foldingRange = {
+  dynamicRegistration = false,
+  lineFoldingOnly = true
+}
+local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+for _, ls in ipairs(language_servers) do
+  require('lspconfig')[ls].setup({
+    capabilities = capabilities
+    -- you can add other fields for setting up lsp server in this table
+  })
+end
+require('ufo').setup()
+
+-- Option 3: treesitter as a main provider instead
+-- (Note: the `nvim-treesitter` plugin is *not* needed.)
+-- ufo uses the same query files for folding (queries/<lang>/folds.scm)
+-- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
+require('ufo').setup({
+  provider_selector = function(bufnr, filetype, buftype)
+    return { 'treesitter', 'indent' }
+  end
+})
+
+-- Option 4: disable all providers for all buffers
+
+require('ufo').setup({
+  provider_selector = function(bufnr, filetype, buftype)
+    return ''
+  end
+})
+--  END VISUAL FOLDING CONFIG
